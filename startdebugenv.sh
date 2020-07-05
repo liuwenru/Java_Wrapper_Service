e41ae449c35db06ddcb9d216ab34ca0581b00987
ijarvis <ijarvis@sina.com>
2020-07-05 16:48:28 +0800

    fix bug

--- startdebugenv.sh
+++ startdebugenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-docker run -it --net=host --privileged=true --name=javawrapperservice -v /root/Tools/apps:/opt/apps   -v $(pwd):/root/Java_Wrapper_Service -e sshport=2225 -e rootpass=liuwenru liuwenru/ubuntu_dev
+docker run -it --net=host --privileged=true --name=javawrapperservice -v /root/tools/apps:/opt/apps   -v $(pwd):/root/Java_Wrapper_Service -e sshport=2225 -e rootpass=liuwenru liuwenru/centos_dev
 
 
