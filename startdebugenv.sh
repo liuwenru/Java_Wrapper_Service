1cd1fcf0e4f13777af12236c36621f8e3daeda3c
liuwenru <ijarvis@sina.com>
2022-04-13 13:47:05 +0800

    fix bug

--- startdebugenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-docker run -d --net=host --privileged=true --name=javawrapperservice -v /root/tools/apps:/opt/apps   -v $(pwd):/root/Java_Wrapper_Service -e sshport=2225 -e rootpass=liuwenru liuwenru/centos_dev
-
-
